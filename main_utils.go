d411a8cf98a8c5797a5994ac5877ab93cf9a259f
nixos <nahyanl63@gmail.com>
2025-05-22 21:28:23 +0500

    added new functions to both main and string utils

---
+++ main_utils.go
@@ -0,0 +1,90 @@
+package humain
+
+import (
+    "fmt"
+    "log"
+    "bufio"
+    "strings"
+    "strconv"
+)
+
+func Input(msg string, val ...any) string {
+    fmt.Printf(msg, val...)
+
+    reader 	:= bufio.NewReader(os.Stdin)
+    data, _	:= reader.ReadString('\n')
+    input	:= strings.TrimSpace(data)
+    return input
+}
+
+func IntSliceContains(slice []int, target int) bool {
+    for _, num := range slice {
+	if num == target {
+	    return true
+	}
+    }
+    return false
+}
+
+func StringSliceContains(slice []string, target string) bool{
+    for _, str := range slice {
+	if str == target {
+	    return true
+	}
+    }
+    return false
+}
+
+func ExitMsg() {
+    fmt.Println("\n\033[1;30;42m Press Enter to exit... \033[0m")
+    xyz := input()
+    if xyz != "" {
+	exit_()
+    }
+    return
+}
+
+func Msg(msg string, color string, fg bool, val ...any) {
+    var scheme int
+    if fg { scheme = 3
+    } else { scheme = 4 }
+
+    if !strings.Contains(color, "#") {
+	var color_code string
+	switch {
+	    case "black": 	color_code = fmt.Sprintf("\033[1;%d0m", scheme)
+	    case "red": 	color_code = fmt.Sprintf("\033[1;%d1m", scheme)
+	    case "green": 	color_code = fmt.Sprintf("\033[1;%d2m", scheme)
+	    case "orange": 	color_code = fmt.Sprintf("\033[1;%d3m", scheme)
+	    case "blue": 	color_code = fmt.Sprintf("\033[1;%d4m", scheme)
+	    case "purple": 	color_code = fmt.Sprintf("\033[1;%d5m", scheme)
+	    case "teal": 	color_code = fmt.Sprintf("\033[1;%d6m", scheme)
+	    case "beige": 	color_code = fmt.Sprintf("\033[1;%d7m", scheme)
+	    default: log.Fatalln(fmt.Sprintf("\033[1;42 Invalid color '%s' given\033[0m", color))
+	}
+
+    } else {
+	parts := strings.Split(color, "#")
+	if len(parts) < 2 { log.Fatalln("Invalid input was given") }
+	hex := parts[len(parts) - 1]
+
+	if len(hex) != 6 { log.Fatalln("Invalid hex code was entered") }
+
+	r64, errR := strconv.ParseInt(hex[0:2], 16, 0)
+	g64, errG := strconv.ParseInt(hex[2:4], 16, 0)
+	b64, errB := strconv.ParseInt(hex[4:6], 16, 0)
+
+	if errR != nil || errG != nil || errB != nil {
+	    log.Fatalln("\033[1;32mParsing Error:\n" +
+			"Unknown value was encountered when parsing hex code\033[0m")
+	}
+
+	r := int(r64)
+	g := int(g64)
+	b := int(b64)
+
+	color_code = fmt.Sprintf("\033[%d8;2;%d;%d;%dm", scheme, r, g, b)
+    }
+
+    fmt.Printf(color_code+msg+"\033[0m", val...)
+}
---
+++ string_utils.go
@@ -0,0 +1,51 @@
+package hustr
+
+import (
+    "strconv"
+)
+
+func I64ToStr(input []int64, type_ int) []string {
+    out := make([]string, len(input))
+    for i, val := range input { out[i] = strconv.FormatInt(val, type_) }
+    return out
+}
+
+func F64ToStr(input []float64, type_ int) []string {
+    out := make([]string, len(input))
+    for i, val := range input { out[i] = strconv.FormatFloat(val, 'f', -1, type_) }
+    return out
+}
+
+func PrettyHttp(response *http.Response) string {
+
+    dump, err := httputil.DumpResponse(response, true)
+    if err != nil { err_("Error dumping response: %v", err) }
+    input := string(dump)
+
+    new_input := []string{}
+    scanner := bufio.NewScanner(strings.NewReader(input))
+    for scanner.Scan() {
+	line := scanner.Text()
+	if line != "" {
+	    new_input = append(new_input, line)
+	}
+    }
+    input = strings.Join(new_input, "\n")
+
+    input = strings.ReplaceAll(input, "\n", "\n    ]")
+    input = strings.ReplaceAll(input, ": ", " [\n        ")
+    input = strings.ReplaceAll(input, ", ", ",\n        ")
+    input = strings.ReplaceAll(input, "]", "], ")
+    input = strings.ReplaceAll(input, "3e", "\n}\n3e")
+
+    lines := strings.SplitN(input, "\n", 2)
+    if len(lines) > 1 {
+	lines[0] 	= fmt.Sprintf("%s {", strings.TrimSpace(lines[0]))
+	new_line2 	:= strings.Replace(lines[1], "], ", "", 1)
+	lines[1] 	= fmt.Sprintf(".   %s", strings.TrimSpace(new_line2))
+	lines[len(lines)-1] = fmt.Sprintf("%s\n}", strings.TrimSpace(lines[len(lines)-1]))
+
+	input 		= strings.Join(lines, "\n")
+    }
+    return input
+}
